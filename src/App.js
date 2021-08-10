7bbf3714053f8cc1b28ddea2c5814e07f99af853
JSantos <jeremy.santos1010@gmail.com>
2021-08-09 23:17:29 -0400

    understanding components and its versatility added expenses to expense tracker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<Expenses />
+		</div>
+	);
 }
 
 export default App;
